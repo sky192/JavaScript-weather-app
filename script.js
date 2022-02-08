a7df886f3031ce2493ab1571d9f0cda95a5545db
Samaheh Loghmani <samahehloghmani@gmail.com>
2022-02-08 15:41:57 -0500

    created addeventListener for submit form

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".msg");
 const list = document.querySelector(".cities");
 const apiKey = "3e431907796601f080ad13c46fc5d988";
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputVal = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const { main, namr, sys, weather } = data;
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+
+            const li = document.createElement("li");
+            li.classList.add("city");
+            const markup = `
+            <h2 class='city-name' data-name=''></h2>
+            `;
+        });
+});
