d5abf6935d4389db1d19dedf229813d70a921128
Samaheh Loghmani <samahehloghmani@gmail.com>
2022-02-08 14:51:20 -0500

    get element from dom

---
+++ script.js
@@ -0,0 +1,7 @@
+// `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+// api.openweathermap.org/data/2.5/weather?q=tehran&appid=3e431907796601f080ad13c46fc5d988&units=imperial
+
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".msg");
+const list = document.querySelector(".cities");
